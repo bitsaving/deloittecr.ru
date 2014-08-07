8a2d6da541996889c450bf312567c6c07b04f428
Sergey <eupathy@gmail.com>
2014-08-07 12:41:38 +0400

    Добавлены социальные сети

--- app/views/extraMile/links.php
+++ app/views/extraMile/links.php
@@ -8,11 +8,16 @@
 			<a href=""> <img src="/img/social_links/extra_mile_link_fb.png"> </a>
 		</div>
 		<div class="share_links pull-right">
-			<span>Рассказать друзьям:</span> <a href=""><img src="/img/social_links/share_vk.png"></a>
+			<span>Рассказать друзьям:</span>
+
+			<div class="share42init"></div>
+			<script type="text/javascript" src="packages/share24/share42.js"></script>
+
+			<!--<a href=""><img src="/img/social_links/share_vk.png"></a>
 			<a href="" target="_blank"><img src="/img/social_links/share_fb.png"></a>
 			<a href="" target="_blank"><img src="/img/social_links/share_ok.png"></a>
 			<a href="" target="_blank"><img src="/img/social_links/share_tw.png"></a>
-			<a href="" target="_blank"><img src="/img/social_links/share_lj.png"></a>
+			<a href="" target="_blank"><img src="/img/social_links/share_lj.png"></a>-->
 		</div>
 	</div>
 </div>
