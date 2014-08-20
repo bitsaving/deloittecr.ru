b0c7aca40a784ab88cf266175a56daa8c28fefe5
Sergey <eupathy@gmail.com>
2014-08-20 10:48:10 +0400

    Начало обработки платежей

--- app/views/donations/payment.php
+++ app/views/donations/payment.php
@@ -8,8 +8,8 @@ if (!isset($teamName)) {
 	<input type="hidden" name="scid" value="56210"> <input type="hidden" name="ShopID" value="18673">
 
 	ФИО Плательщика:<br> <input type=text name="CustomerNumber" size="43" required="required"><br><br>
-	Сумма:<br><input type=text name="Sum" size="43" required="required"><br><br> Команда:<br><input type="text" name="teamName" value="<?= $teamName ?>" disabled="disabled"><br><br> Способ
-	оплаты:<br><br> <input name="paymentType" value="PC" type="radio" checked="checked" />Со счета в
+	Сумма:<br><input type=text name="Sum" size="43" required="required"><br><br>
+	Команда:<br><input type="text" name="teamName" value="<?= $teamName ?>"><br><br> Способ оплаты:<br><br> <input name="paymentType" value="PC" type="radio" checked="checked" />Со счета в
 	Яндекс.Деньгах<br /> <input name="paymentType" value="AC" type="radio" />С банковской карты<br />
 	<input name="paymentType" value="WM" type="radio" />Со счета WebMoney<br />
 	<input name="paymentType" value="GP" type="radio" />По коду через терминал<br /><br />
