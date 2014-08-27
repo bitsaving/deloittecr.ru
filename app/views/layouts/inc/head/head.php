af483d508304ed43fb1789e0618735f0668a0a23
Sergey <eupathy@gmail.com>
2014-08-28 01:04:44 +0400

    Пробуем отображать правильное изображение при кросспостинге на  фейсбук

--- app/views/layouts/inc/head/head.php
+++ app/views/layouts/inc/head/head.php
@@ -1,6 +1,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<meta property="og:url" content="http://deloittecr.ru/extramile" />
 <meta property="og:title" content="ExtraMile 2014" />
-<meta property="og:description" content="" />
+<meta property="og:description" content="Денежные средства для благотворительного фонда собираются не только участниками мероприятия и организациями, но и просто небезразличными людьми ― вы также можете присоединиться к ним и помочь детям!" />
 <meta property="og:image" content="http://deloittecr.ru/img/title/title_bg_with_logo_icons_old.jpg" />
 
 <title>ExtraMile 2014</title>
